bac242002cacdc879e6ae64115ce1935706f5f0c
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2021-04-25 12:56:39 -0700

    chat app

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,57 @@
 package _02_Chat_Application;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import _00_Click_Chat.gui.ButtonClicker;
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
-
+public class ChatApp extends JFrame{
+	JButton button = new JButton("CLICK");
+	
+	Server chatServer;
+	Client chatClient;
+	
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp(){
+		
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!", JOptionPane.YES_NO_OPTION);
+		if(response == JOptionPane.YES_OPTION){
+			chatServer = new Server(8080);
+			setTitle("SERVER");
+			JOptionPane.showMessageDialog(null, "Server started at: " + chatServer.getIPAddress() + "\nPort: " + chatServer.getPort());
+			button.addActionListener((e)->{
+				chatServer.sendClick();
+			});
+			add(button);
+			setVisible(true);
+			setSize(400, 300);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			chatServer.start();
+			
+		}else{
+			setTitle("CLIENT");
+			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
+			String prtStr = JOptionPane.showInputDialog("Enter the port number");
+			int port = Integer.parseInt(prtStr);
+			chatClient = new Client(ipStr, port);
+			button.addActionListener((e)->{
+				chatClient.sendClick();
+			});
+			add(button);
+			setVisible(true);
+			setSize(400, 300);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			chatClient.start();
+		}
+	}
 }
